a9f042856d2ca37b6a267d33331155a8a7042f93
Benji Asperheim <basperheim@gmail.com>
2023-11-15 03:15:41 +0800

    Adds copy of modal for sliding modal (needs changing)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { PopupModalComponent } from "./popup-modal/popup-modal.component";
+import { SlidingModalComponent } from "./sliding-modal/sliding-modal.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
@@ -14,6 +15,14 @@ export class AppComponent {
   openPopupModal(): void {
     // Open the dialog using MatDialog
     this.dialog.open(PopupModalComponent, {
+      width: "400px",
+      data: { message: "Your custom message here" },
+    });
+  }
+
+  openSlidingModal(): void {
+    // Open the dialog using MatDialog
+    this.dialog.open(SlidingModalComponent, {
       width: "400px",
       data: { message: "Your custom message here" },
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PopupModalComponent } from "./popup-modal/popup-modal.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SlidingModalComponent } from './sliding-modal/sliding-modal.component';
 
 @NgModule({
-  declarations: [AppComponent, PopupModalComponent],
+  declarations: [AppComponent, PopupModalComponent, SlidingModalComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatDialogModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
